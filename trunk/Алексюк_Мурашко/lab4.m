568ffb901456fb982ece8a359dd9a38aea83d4a8
ArtyomAleksuk <ArtyomAleksuk@0eaeb0c7-c5c1-4de7-b801-81c1bd771a3d>
2014-03-13 21:12:18 +0000

    Лаба 3,4

--- trunk/Алексюк_Мурашко/lab4.m
+++ trunk/Алексюк_Мурашко/lab4.m
@@ -4,11 +4,11 @@ x = 0:0.01:4*pi;
 f0 = 5;
 %исходный сигнал
 y = sin(2*pi*f0*x);
-plot(x,y)
+plot(x(1:200),y(1:200))
 grid
 %спектр исходного сигнала 
 figure
-spectrum = abs(fft(y,1024)); 
+spectrum = abs(fft(y,128)); 
 norm_spectrum = spectrum.*conj(spectrum)/512;
 f=100*(0:127)/512;
 plot(f, norm_spectrum(1:128))
