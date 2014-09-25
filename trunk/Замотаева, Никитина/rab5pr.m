92ef2c6d73ba4f938c2d0f7d084ecd8242b57a3b
vilegzhanina <vilegzhanina@0eaeb0c7-c5c1-4de7-b801-81c1bd771a3d>
2014-09-25 17:40:15 +0000

    deleted

--- trunk/Замотаева, Никитина/rab5pr.m
+++
@@ -1,6 +0,0 @@
-t=-0.04:1/1000:0.04;
-y4=-5*rectpuls(t,0.04);
-plot(t,y4);
-figure;
-spectrum = abs(fft(y4,1024))/1024;
-plot(spectrum);
--- trunk/Замотаева, Никитина/rab5tr.m
+++
@@ -1,8 +0,0 @@
-t=-0.04:1/1000:0.04;
-y4=-5*rectpuls(t,0.02);
-y5=-10*rectpuls(t,0.03);
-y6= conv(y4,y5);
-plot(y6);
-figure;
-spectrum = abs(fft(y6,1024))/1024;
-plot(spectrum);
