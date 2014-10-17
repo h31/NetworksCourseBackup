2ae87941b265b4a4acfcfea325eb9e4cf34bb9e3
Pennywise <Pennywise@0eaeb0c7-c5c1-4de7-b801-81c1bd771a3d>
2014-10-17 15:33:58 +0000

    New server

--- Baratynskiy/NewFileTransfer/Server.c
+++ Baratynskiy/NewFileTransfer/Server.c
@@ -9,15 +9,71 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
+
+void getFileName(FILE *f,int sock, int bufsocket,struct sockaddr_in clientAddr,int clilen,pid_t pid)
+{
+	bufsocket = accept(sock, (struct sockaddr *)&clientAddr,
+	                                &clilen);
+	    if (bufsocket < 0)
+	    {
+	        perror("ERROR on accept");
+	        exit(1);
+	    }
+	    //create child process
+	    pid = fork();
+	            if (pid < 0)
+	            {
+	                perror("ERROR on fork");
+	    	    exit(1);
+	            }
+	            if (pid == 0)
+	            {
+	                /* This is the client process */
+	                close(sock);
+	                doprocessing(bufsocket,f);
+	                fprintf(f,"%s\n","");
+	                exit(0);
+	            }
+	            else
+	            {
+	                close(bufsocket);
+	            }
+}
+
+void doprocessing (int sock,FILE *f)
+{
+    int n;
+    char buffer[256];
+
+    bzero(buffer,256);
+
+    n = read(sock,buffer,255);
+    if (n < 0)
+    {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+    fprintf(f,"%s",buffer);
+    if (n < 0)
+    {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+}
 
 int main( int argc, char *argv[] )
 {
     int sock, bufsocket, port, clilen;
     char buffer[256];
     struct sockaddr_in serverAddr, clientAddr;
-    int  n;
-    FILE *file = fopen("file.txt","ab");
+    int  n,b;
+    int size;
+    int i=0;
+    pid_t pid;
+    FILE *f = fopen("file.txt","ab");
+    struct stat buff;
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0)
     {
@@ -26,7 +82,7 @@ int main( int argc, char *argv[] )
     }
     //Initializing socket
     bzero((char *) &serverAddr, sizeof(serverAddr));
-    port = 5003;
+    port = 9999;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(port);
@@ -42,8 +98,9 @@ int main( int argc, char *argv[] )
     //Listening for clients
     listen(sock,5);
     clilen = sizeof(clientAddr);
-
+    getFileName(f,sock,bufsocket,clientAddr,clilen,pid);
     //Accept connection
+    while(1){
     bufsocket = accept(sock, (struct sockaddr *)&clientAddr,
                                 &clilen);
     if (bufsocket < 0)
@@ -51,24 +108,29 @@ int main( int argc, char *argv[] )
         perror("ERROR on accept");
         exit(1);
     }
-    //Communication
-    bzero(buffer,256);
-    n = read( bufsocket,buffer,255 );
-    if (n < 0)
-    {
-        perror("ERROR reading from socket");
-        exit(1);
+    //create child process
+    pid = fork();
+            if (pid < 0)
+            {
+                perror("ERROR on fork");
+    	    exit(1);
+            }
+            if (pid == 0)
+            {
+                /* This is the client process */
+                close(sock);
+                doprocessing(bufsocket,f);
+                exit(0);
+            }
+            else
+            {
+                close(bufsocket);
+            }
     }
-    fprintf(file,"%s",buffer);
-    printf("The file was received: \n");
+    //fprintf(file,"%s",buffer);
+ //   printf("The file was received: \n");
 
-    fclose(file);
-    //Response to client
-    n = write(bufsocket,"I got your file\n",18);
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
+    fclose(f);
+
     return 0;
 }
