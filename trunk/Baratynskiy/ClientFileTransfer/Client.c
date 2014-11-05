2c131e28c0241f9d89863c42f5877b0020df35c2
Pennywise <Pennywise@0eaeb0c7-c5c1-4de7-b801-81c1bd771a3d>
2014-11-05 16:05:48 +0000

    More changes

--- trunk/Baratynskiy/ClientFileTransfer/Client.c
+++ trunk/Baratynskiy/ClientFileTransfer/Client.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 
 #define bufSize 1024
 
@@ -44,6 +46,11 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 	write(sock, user, sizeof(user));
+	int ch = chdir(user);
+	if (ch == -1) {
+		mkdir(user, S_IRWXU);
+		chdir(user);
+	}
 	bzero(user, sizeof(user));
 	read(sock, buffer, bufSize - 1);
 	bzero(buffer, bufSize);
@@ -53,7 +60,6 @@ int main(int argc, char *argv[]) {
 			firstTime = 1;
 			printf("Enter file name: ");
 			scanf("%s", fileName);
-			puts(fileName);
 			FILE *file = fopen(fileName, "r+");
 			fseek(file, 0, SEEK_END);
 			size = ftell(file);
@@ -163,6 +169,52 @@ int main(int argc, char *argv[]) {
 				printf("%s", buffer);
 				write(sock,"o",1);
 			}
+
+		} else if (strcmp(commandLine, "get") == 0) {
+			char filename[30];
+			printf("Get ");
+			scanf("%s",filename);
+			sock = socket(AF_INET, SOCK_STREAM, 0);
+						if (sock < 0) {
+							perror("ERROR opening socket");
+							exit(1);
+						}
+						server = gethostbyname(argv[1]);
+						if (server == NULL) {
+							fprintf(stderr, "ERROR, no such host\n");
+							exit(0);
+						}
+
+						bzero((char *) &serv_addr, sizeof(serv_addr));
+						serv_addr.sin_family = AF_INET;
+
+						serv_addr.sin_port = htons(port);
+
+						/* Now connect to the server */
+						if (connect(sock, &serv_addr, sizeof(serv_addr)) < 0) {
+							perror("ERROR connecting");
+							exit(1);
+						}
+
+						write(sock, "get", 3);
+						bzero(buffer, bufSize);
+						read(sock, buffer, bufSize - 1);
+						write(sock,filename,sizeof(filename));
+						FILE *f = fopen(filename,"ab");
+						bzero(buffer, bufSize);
+						read(sock, buffer, bufSize - 1);
+						while(1){
+							bzero(buffer,bufSize);
+							read(sock,buffer,bufSize-1);
+							if(strcmp(buffer,"&")==0){
+								fclose(f);
+								break;
+							}
+							else{
+								fprintf(f, "%s", buffer);
+								write(sock,"m",1);
+							}
+						}
 
 		} else if (strcmp(commandLine, "close") == 0) {
 			return 0;
