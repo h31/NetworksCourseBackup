a57025542f9140fd4f4fdb49f66526eb250c5cec
Anton.Kiselev.94.10813 <Anton.Kiselev.94.10813@0eaeb0c7-c5c1-4de7-b801-81c1bd771a3d>
2014-11-06 22:03:27 +0000

    Nebolshie izmeneniya, i nachato dlya windows

--- Anton Kiselev/client.c
+++ Anton Kiselev/client.c
@@ -60,38 +60,49 @@ int main(int argc, char *argv[])
 	 if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)//Подсоединение к серверу
 	      error("ERROR connecting");
 
-	 printf("Please enter the message: ");//Введите сообщение
-	 bzero(buffer,256);
-	 char passbuf[256];
-	 bzero(passbuf,256);
-	 fgets(buffer,256,stdin);//Запись сообщения от пользователоя в массив
-	 n = write(sockfd,buffer,strlen(buffer)-1);//Отправка сообщения на сервер
-	 strncpy(passbuf,buffer,strlen(buffer)-1);
-	 if (n < 0)
-		 error("ERROR writing to socket");
-	 //do//
-	 //{
-	 bzero(buffer,256);
-	 n = read(sockfd,buffer,255);//Чтение полученного сообщения
-     printf("%s\n",buffer);
-     printf("%s, %i\n",passbuf,strlen(passbuf));
+     char passbuf[256];
+     int is_auntentity_ok = 0;
+     //do
+     //{
+    	 bzero(buffer,256);
+    	 n = read(sockfd,buffer,255);//Чтение полученного сообщения
+    	 printf("%s\n",buffer);
+    	 printf("Please enter the message: ");//Введите сообщение
+    	 bzero(buffer,256);
+    	 bzero(passbuf,256);
+    	 fgets(buffer,256,stdin);//Запись сообщения от пользователоя в массив
+    	 n = write(sockfd,buffer,strlen(buffer)-1);//Отправка сообщения на сервер
+    	 strncpy(passbuf,buffer,strlen(buffer)-1);
+    	 if (n < 0)
+    		 error("ERROR writing to socket");
+    	 bzero(buffer,256);
+    	 n = read(sockfd,buffer,255);//Чтение полученного сообщения
+    	 printf("%s\n",buffer);
+    	 if(strcmp(buffer,"Polzovatel' and Parol' neverny!") == 0)
+    	 {
+    		 is_auntentity_ok = 1;
+    	 }
+    // }while(is_auntentity_ok != 1);
+     is_auntentity_ok = 0;
+     //do
+     //{
+    	 unsigned int res = HashH37(passbuf);
+    	 printf("%i\n",res);
+    	 bzero(buffer,256);
+    	 memcpy(buffer, (char*)&res, 3);
+    	 n = write(sockfd,buffer,256);//Отправка сообщения на сервер
+    	 if (n < 0)
+    		 error("ERROR writing to socket");
+    	 bzero(buffer,256);
+    	 n = read(sockfd,buffer,255);//Чтение полученного сообщения
+    	 if (n < 0)
+    		 error("ERROR reading from socket");
+    	 printf("%s\n",buffer);//Выведение сообщения на экран
+    	 if(strcmp(buffer,"Hello!") == 0)
+    		 is_auntentity_ok = 1;
 
-	 unsigned int res = HashH37(passbuf);
-	 printf("%i\n",res);
-	 printf("%i\n",(void*)res);
-	 bzero(buffer,256);
-	 memcpy(buffer, (char*)&res, 3);
-	 n = write(sockfd,buffer,256);//Отправка сообщения на сервер
-	 if (n < 0)
-		 error("ERROR writing to socket");
-	 bzero(buffer,256);
-	 n = read(sockfd,buffer,255);//Чтение полученного сообщения
-	 if (n < 0)
-	     error("ERROR reading from socket");
-	 printf("%s\n",buffer);//Выведение сообщения на экран
-
-	 	 //} while(n >= 0);
-
+    // }
+    // while(is_auntentity_ok != 1);
 	 int i = 0;
 	 for(i = 0; i < 10; i++)
 	 {
@@ -105,6 +116,7 @@ int main(int argc, char *argv[])
 		 n = read(sockfd,buffer,255);//Чтение полученного сообщения
 		 if (n < 0)
 		     error("ERROR reading from socket");
+
 		 printf("%s\n",buffer);//Выведение сообщения на экран
 	  }
       close(sockfd);//Закрытие сокета
