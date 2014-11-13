d64743ad0a0816a1368d12d135e55a5e117d7542
NikitaKent <NikitaKent@0eaeb0c7-c5c1-4de7-b801-81c1bd771a3d>
2014-11-13 20:10:23 +0000

    server ok!

--- trunk/kent/Roulette/roulette.c
+++ trunk/kent/Roulette/roulette.c
@@ -8,132 +8,192 @@
 //#include <stdlib.h>
 //#include <unistd.h>
 #include <string.h>
+#include <time.h>
 //#include <sys/types.h>
 //#include <sys/socket.h>
 //include <netinet/in.h>
 #include <netdb.h>
-int char_to_int (char *ch)
-{
-	int n;
-	int i;
-	char q[3]="123";
+#include <pthread.h>
+pthread_t t1, t2, t3;
 
-	n=atoi(q);
-	return n;
-}
-void game (int sock)
-{
-	int n;
-	char color, line;
-	int number, bet;
+
+void* thread2(int sock) {
 	char buffer[256];
+	char buffer_file[256];
+	int N = 256;
+	int n;
+	int c;
+	int i = 0;
+	int player;
+	int number, odd;
+	char my_number [3];
+	char my_odd_or_even;
+	char my_bet [5];
+	bzero(buffer, N);
 
-	int k;
-	n = write (sock,"Greetings and welcome!I want to play a game.\n",44);
-	n= write (sock,"Enter your bet",14);
-	bzero (buffer, 256);
-	n= read (sock,buffer,255);
-	bet =atoi (buffer);
-	n=write(sock, "Bets are made, no more bets! Please, enter a number\n",51);
-	bzero (buffer,256);
-	n= read (sock, buffer, 255);
-	number=atoi (buffer);
-	n= write (sock, "Ok! Now enter a color",21);
-	bzero (buffer, 256);
-	n= read (sock, buffer, 255);
-	color ="24";
-	printf ("%d -bet , %d- number, %s - color\n",bet, number, color);
-//	printf("%d\n", atoi(buffer));
-	/*here i cant recieve bet
-	bzero(buffer,256);
-	n=resv (sock,buffer,256,0);
-	printf("%s\n", buffer);*/
-//	n=read(sock, buffer,255);
-//	number = rand()% 10;
-//	n=write (sock,"%d\n",number,2);
+	n = read(sock, buffer, N - 1);
+	printf("Here is the message: %s\n", buffer);
+	FILE * fo;
+
+	fo = fopen("test.txt", "r");
+	bzero(buffer_file, N);
+	fgets(buffer_file, 50, fo);
+
+	fclose(fo);
+
+	if (buffer_file[2] == '0') {
+		if (buffer[2] == '1') {
+			fo = fopen("test.txt", "a+");
+			fputs("\n", fo);
+			fputs(buffer, fo);
+			fclose(fo);
+			player = 1;
+			write(sock, "You are in game, player\n", 24);
+		} else
+			write(sock, "u cant be a croup\n", 18);
+	} else {
+		if (buffer[2] == '0') {
+			fo = fopen("test.txt", "a+");
+
+			fputs(buffer, fo);
+			fclose(fo);
+			player = 0;
+			write(sock, "You are in game, croup\n", 23);
+		} else {
+			write(sock, "u cant be a player", 18);
+		}
+
+	}
+	bzero(buffer, N);
+	if (player == 0)
+		write(sock, "waiting...players are making bets\n", 34);
+	else
+		write(sock, "make your bet", 13);
+
+	n = read(sock, buffer, N - 1);
+	printf("Here is the message: buffer[0]= %c, player= %d\n", buffer[0],
+			player);
+	if (player == 0) {
+		if (buffer[0] == "1")
+			printf("its time to show bets\n");
+		fo = fopen("bets.txt", "r");
+		while (fgets(buffer, 50, fo) != NULL) {
+			printf("bets: %s\n", buffer);
+		}
+	} else {
+		fo = fopen("bets.txt", "a+");
+		fputs(buffer, fo);
+		fputs("\n", fo);
+		fclose(fo);
+		while (buffer[i]!=';') {
+			my_number[i]=buffer[i];
+					i++;
+		}
+		i++;
+		my_odd_or_even=buffer[i];
+		i++;
+		int j=0;
+		for(i=5;i <10;i++,j++) {
+			my_bet[j]=buffer[i];
+
+		}
+
+	}
+
+	if (player == 0) {
+		bzero(buffer, N);
+		n = read(sock, buffer, N - 1);
+		if (buffer[0] == 's') {
+			srand(time(NULL));
+			number = rand() % 100;
+			odd = rand() % 2;
+			fo = fopen("game.txt", "a+");
+			fprintf(fo, "%d", number);
+			fputs("\n", fo);
+			fprintf(fo, "%d", odd);
+			fclose(fo);
+		}
+
+	}
+	int win1=5;
+	int win2=5;
+	if (player == 1) {
+		n = read(sock, buffer, N - 1);
+		if (buffer[0] == 'r') {
+			printf("new check\n");
+
+			fo = fopen("game.txt", "r");
+			fgets(buffer, 50, fo);
+
+			if (strcmp(buffer,my_number)==0)
+				win1=1;
+
+			else
+				win1=0;
+			fgets(buffer, 50, fo);
+			if (buffer[0]==my_odd_or_even)
+							win2=1;
+
+						else
+							win2=0;
+
+			fclose(fo);
+			if (win1==1||win2==1)
+				write(sock, "Granz, you won", 14);
+			else
+				write(sock, "you loose", 9);
+		}
+	}
+
 }
-void doprocessing (int sock)
-{
-    int n;
-    int rand;
-    char buffer[256];
-    game(sock);
-    bzero(buffer,256);
-
-    n = read(sock,buffer,255);
-    if (n < 0)
-    {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-    printf("Here is the message: %s\n",buffer);
-    n = write(sock,"I got your message",18);
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
+void* thread1(int sock) {
+	write(sock, "its thread1", 11);
+//	printf("its thread\n");
 }
-int main( int argc, char *argv[] )
-{
-    int sockfd, newsockfd, portno, clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    int  n;
-    pid_t pid;
-    /* First call to socket() function */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("ERROR opening socket");
-        exit(1);
-    }
-    /* Initialize socket structure */
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = 9999;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+int main(int argc, char *argv[]) {
+	int sockfd, newsockfd, portno, clilen, client;
+	char buffer[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
 
-    /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                          sizeof(serv_addr)) < 0)
-    {
-         perror("ERROR on binding");
-         exit(1);
-    }
-    /* Now start listening for the clients, here
-     * process will go in sleep mode and will wait
-     * for the incoming connection
-     */
-    listen(sockfd,5);
-    clilen = sizeof(cli_addr);
-    while (1)
-    {
-        newsockfd = accept(sockfd,
-                (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0)
-        {
-            perror("ERROR on accept");
-            exit(1);
-        }
-        /* Create child process */
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("ERROR on fork");
-	    exit(1);
-        }
-        if (pid == 0)
-        {
-            /* This is the client process */
-            close(sockfd);
-            doprocessing(newsockfd);
-            exit(0);
-        }
-        else
-        {
-            close(newsockfd);
-        }
-    } /* end of while */
+	/* First call to socket() function */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		perror("ERROR opening socket");
+		exit(1);
+	}
+	/* Initialize socket structure */
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = 5001;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+	const int on = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+	/* Now bind the host address using bind() call.*/
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("ERROR on binding");
+		exit(1);
+	}
+
+	/* Now start listening for the clients, here process will
+	 * go in sleep mode and will wait for the incoming connection
+	 */
+
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
+	bzero(buffer, 256);
+
+	while (1) {
+
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0) {
+			perror("ERROR on accept");
+			exit(1);
+		}
+
+		pthread_create(&t2, NULL, thread2, newsockfd);
+
+	}
+	return 0;
 }
